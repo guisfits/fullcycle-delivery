6cb5faf68ffd8b6e80d33240e1e5a0353e01e33e
Guilherme Camargo <guisfits@gmail.com>
2023-02-07 21:19:18 -0300

    consumer and producer

---
+++ infra/kafka/consumer.go
@@ -0,0 +1,33 @@
+package kafka
+
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"log"
+	"os"
+)
+
+type KafkaConsumer struct {
+	MsgChan chan *ckafka.Message
+}
+
+func (k *KafkaConsumer) Consume() {
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": os.Getenv("KafkaBootstrapServers"),
+		"group.id":          os.Getenv("KafkaConsumerGroupId"),
+	}
+
+	c, err := ckafka.NewConsumer(configMap)
+	if err != nil {
+		log.Fatalf("error consuming kafka message: " + err.Error())
+	}
+
+	topics := []string{os.Getenv("KafkaReadTopic")}
+	c.SubscribeTopics(topics, nil)
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			k.MsgChan <- msg
+		}
+	}
+}
